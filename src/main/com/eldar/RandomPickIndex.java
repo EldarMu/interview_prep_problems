038b92f197f922cca930c2548fc2fdb7aa593572
eldar <eldarmu@live.com>
2018-10-09 19:17:45 +0300

    Added "Random Pick Index" class and tests

---
+++ src/main/com/eldar/RandomPickIndex.java
@@ -0,0 +1,33 @@
+package com.eldar;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+//given an array of integers with possible duplicates
+//the class needs to, when the get(int n) method is called,
+//return a random instance of that integer out of its duplicates
+//needs to take up less than O(N) space as the input array may be very large
+//https://leetcode.com/problems/random-pick-index/description/
+//sorting the array is not allowed, indices must remain as provided
+//solution beats 62% of java submissions
+public class RandomPickIndex {
+  private int[] nums;
+  private Random rand;
+
+  public RandomPickIndex(int[] nums){
+    if(nums==null||nums.length==0) throw new IllegalArgumentException("must input a valid array");
+    this.nums=nums;
+    rand = new Random();
+  }
+  public int pick(int target) {
+    List<Integer> instancesOfTarget = new ArrayList<>();
+    for(int i=0; i<nums.length; i++){
+      if(nums[i]==target) instancesOfTarget.add(i);
+    }
+    if(instancesOfTarget.isEmpty()) return -1;
+    return instancesOfTarget.get(rand.nextInt(instancesOfTarget.size()));
+  }
+
+}
---
+++ src/test/com/eldar/RandomPickIndexTest.java
@@ -0,0 +1,41 @@
+package com.eldar;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class RandomPickIndexTest {
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testInputValidation(){
+    RandomPickIndex rpi = new RandomPickIndex(null);
+    RandomPickIndex rpj = new RandomPickIndex(new int[] {});
+  }
+
+  @Test
+  public void RandomPickIndex() throws Exception{
+    RandomPickIndex rpi;
+    int[] input;
+    int targetVal;
+    Set<Integer> validResults = new HashSet<>();
+
+    input = new int[] {1,2,3,3,3};
+    rpi = new RandomPickIndex(input);
+
+    targetVal = 1;
+    validResults.add(0);
+    Assert.assertTrue(validResults.contains(rpi.pick(targetVal)));
+
+    targetVal = 3;
+    validResults.clear();
+    validResults.add(2);
+    validResults.add(3);
+    validResults.add(4);
+    Assert.assertTrue(validResults.contains(rpi.pick(targetVal)));
+    Assert.assertTrue(validResults.contains(rpi.pick(targetVal)));
+    Assert.assertTrue(validResults.contains(rpi.pick(targetVal)));
+  }
+
+}
